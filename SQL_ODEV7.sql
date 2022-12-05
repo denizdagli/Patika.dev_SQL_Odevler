3fc84439435593327e6293d89fe563a5db3b7892
deniz dagli <101983495+denizdagli@users.noreply.github.com>
2022-12-05 07:11:14 +0300

    Add files via upload

---
+++ SQL_ODEV7.sql
@@ -0,0 +1,20 @@
+film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran 
+country_id bilgisini ve şehir sayısını paylaşınız.
+
+SELECT rating From film
+GROUP BY rating ;
+
+SELECT replacement_cost, COUNT(*) AS ADET From film
+GROUP BY replacement_cost
+HAVING (COUNT(*)>50);
+
+SELECT StoreID, COUNT(*) AS ADET From Sales.Customer
+GROUP BY StoreID;
+
+SELECT country_id, COUNT(*) AS ADET From city
+GROUP BY country_id
+ORDER BY ADET DESC
+LIMIT 1;
