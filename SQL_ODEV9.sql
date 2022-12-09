205f21fa7015a0f28b091c968015b9c4f69e3ee4
deniz dagli <101983495+denizdagli@users.noreply.github.com>
2022-12-09 11:07:17 +0300

    Update SQL_ODEV9.sql

--- SQL_ODEV9.sql
+++ SQL_ODEV9.sql
@@ -2,6 +2,6 @@
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 
-SELECT city, country FROM city JOIN country ON city.city_id=country.city_id;
-SELECT payment_id, customer.first_name, customer.last_name FROM payment JOIN customer ON customer.payment_id=payment.payment_id;
-SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental JOIN customer ON customer.rental_id=rental.rental_id;
+SELECT city, country FROM city INNER JOIN country ON city.city_id=country.city_id;
+SELECT payment_id, customer.first_name, customer.last_name FROM payment INNER JOIN customer ON customer.payment_id=payment.payment_id;
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental INNER JOIN customer ON customer.rental_id=rental.rental_id;
